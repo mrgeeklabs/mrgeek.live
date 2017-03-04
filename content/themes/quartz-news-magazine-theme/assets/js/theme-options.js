ad00ff6a4473f9eb074afbbc638aa7df5a120313
Ali Gajani <aligajani@gmail.com>
2017-03-04 22:02:56 +0000

    My first commit

---
+++ content/themes/quartz-news-magazine-theme/assets/js/theme-options.js
@@ -0,0 +1,38 @@
+/**
+ * You can configure theme options in this file.
+ * Please refer to the documentation for more information.
+ */
+
+/************** Comments **************/
+
+var disqus = "mr-geek-live";
+
+/************** Lightbox **************/
+
+var lightbox = true;
+
+/*********** Infinite Scroll **********/
+
+var infinite_scroll = "";
+
+/******** Featured Posts Slider ********/
+
+var slider_auto_play_speed = 4000;
+var slider_wrap_around = true;
+var slider_draggable = true;
+var slider_nav = true;
+
+/************ Social Media ************/
+
+var google_plus_link = "https://plus.google.com/";
+var dribbble_link = "";
+var instagram_link = "";
+var tumblr_link = "";
+var youtube_link = "";
+var vimeo_link = "";
+var pinterest_link = "";
+var flickr_link = "";
+var linkedin_link = "";
+var github_link = "";
+var soundcloud_link = "";
+var rss_link = false;
